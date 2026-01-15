3f3566ddf3128e92ea0f2f42d03600a3864e46b1
Viktor Kalistratov <viktor.calistratov27@gmail.com>
2026-01-15 17:31:27 +0300

    Dellete all

--- api/core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from core import views
-
-urlpatterns = [
-    path("api/scan-runs", views.create_scan_run),
-    path("api/scan-runs/<int:scan_run_id>", views.get_scan_run),
-]
