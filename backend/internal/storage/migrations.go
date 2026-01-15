1d361c82b8a86e2e905eb17740de92ebb617fa16
Viktor Kalistratov <viktor.calistratov27@gmail.com>
2026-01-16 01:37:37 +0300

    1234

--- backend/internal/storage/migrations.go
+++ backend/internal/storage/migrations.go
@@ -1,21 +1,35 @@
 package storage
 
 import (
-	"database/sql"
-	"errors"
+    "database/sql"
+    "errors"
+    "strings"
 
-	"github.com/golang-migrate/migrate/v4"
-	"github.com/golang-migrate/migrate/v4/database/postgres"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
+    "github.com/golang-migrate/migrate/v4"
+    "github.com/golang-migrate/migrate/v4/database/postgres"
+    _ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
+
 func RunMigrations(db *sql.DB, migrationsPath string) error {
+	if migrationsPath == "" {
+		return errors.New("migrations path is empty")
+	}
+
+	if !strings.HasPrefix(migrationsPath, "file://") {
+		migrationsPath = "file://" + migrationsPath
+	}
+
 	driver, err := postgres.WithInstance(db, &postgres.Config{})
 	if err != nil {
 		return err
 	}
 
-	migrator, err := migrate.NewWithDatabaseInstance(migrationsPath, "postgres", driver)
+	migrator, err := migrate.NewWithDatabaseInstance(
+		migrationsPath,
+		"postgres",
+		driver,
+	)
 	if err != nil {
 		return err
 	}
