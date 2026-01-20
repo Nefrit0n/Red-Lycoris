09e5511134bf0f7169b28a750c4763da5aaa285c
Viktor Kalistratov <viktor.calistratov27@gmail.com>
2026-01-21 02:45:11 +0300

    1234

--- backend/migrations/012_vuln_intel.down.sql
+++ backend/migrations/012_vuln_intel.down.sql
@@ -1,2 +1,17 @@
-DROP TABLE IF EXISTS finding_vuln_identifiers;
-DROP TABLE IF EXISTS vuln_intel;
+BEGIN;
+
+DROP INDEX IF EXISTS idx_findings_vulnerability_intel_id;
+DROP INDEX IF EXISTS idx_vuln_intel_vulnerability_id;
+
+ALTER TABLE findings
+    DROP COLUMN IF EXISTS vulnerability_intel_id;
+
+DROP TABLE IF EXISTS vulnerability_intel;
+
+ALTER TABLE findings
+    DROP COLUMN IF EXISTS raw_data;
+
+ALTER TABLE findings
+    DROP COLUMN IF EXISTS evidence;
+
+COMMIT;
