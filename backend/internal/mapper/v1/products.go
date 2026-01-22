cc5a21aa4dc615d35ff8226fc9768bfd8215e2f0
Viktor Kalistratov <64070252+Nefrit0n@users.noreply.github.com>
2026-01-22 22:51:20 +0300

    Add v1 DTOs and mappers for API responses

---
+++ backend/internal/mapper/v1/products.go
@@ -0,0 +1,79 @@
+package v1
+
+import (
+	"time"
+
+	v1dto "lotus-warden/backend/internal/dto/v1"
+	"lotus-warden/backend/internal/models"
+	"lotus-warden/backend/internal/storage"
+)
+
+func ProductListItem(item storage.ProductListItem) v1dto.ProductListItemDTO {
+	var identifier *string
+	if item.Identifier.Valid {
+		value := item.Identifier.String
+		identifier = &value
+	}
+	var version *string
+	if item.Version.Valid {
+		value := item.Version.String
+		version = &value
+	}
+	var assetCriticality *string
+	if item.AssetCriticality.Valid {
+		value := item.AssetCriticality.String
+		assetCriticality = &value
+	}
+	var lastScanAt *string
+	if item.LastScanAt.Valid {
+		value := item.LastScanAt.Time.Format(time.RFC3339)
+		lastScanAt = &value
+	}
+	return v1dto.ProductListItemDTO{
+		ID:                item.ID.String(),
+		Name:              item.Name,
+		Identifier:        identifier,
+		Version:           version,
+		AssetCriticality:  assetCriticality,
+		LastScanAt:        lastScanAt,
+		FindingsOpenCount: item.FindingsOpenCount,
+	}
+}
+
+func ProductDetailFromListItem(item storage.ProductListItem) v1dto.ProductDetailDTO {
+	return v1dto.ProductDetailDTO{
+		ProductListItemDTO: ProductListItem(item),
+	}
+}
+
+func ProductDetail(item models.Product, findingsOpenCount int, lastScanAt *time.Time) v1dto.ProductDetailDTO {
+	var identifier *string
+	if item.Identifier != nil && *item.Identifier != "" {
+		identifier = item.Identifier
+	}
+	var version *string
+	if item.Version != nil && *item.Version != "" {
+		version = item.Version
+	}
+	var assetCriticality *string
+	if item.AssetCriticality != nil && *item.AssetCriticality != "" {
+		assetCriticality = item.AssetCriticality
+	}
+	var lastScanAtStr *string
+	if lastScanAt != nil {
+		value := lastScanAt.Format(time.RFC3339)
+		lastScanAtStr = &value
+	}
+	return v1dto.ProductDetailDTO{
+		ProductListItemDTO: v1dto.ProductListItemDTO{
+			ID:                item.ID.String(),
+			Name:              item.Name,
+			Identifier:        identifier,
+			Version:           version,
+			AssetCriticality:  assetCriticality,
+			LastScanAt:        lastScanAtStr,
+			FindingsOpenCount: findingsOpenCount,
+		},
+		Description: item.Description,
+	}
+}
