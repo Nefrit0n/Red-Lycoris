a995e0cfb237f0cc837ff7823ea92765ac4981db
Viktor Kalistratov <viktor.calistratov27@gmail.com>
2026-02-08 18:53:32 +0300

    python fix

--- python_api/gunicorn.conf.py
+++ python_api/gunicorn.conf.py
@@ -1,7 +1,7 @@
 import os
 
 bind = f"0.0.0.0:{os.getenv('PYTHON_API_PORT', '8000')}"
-workers = int(os.getenv('GUNICORN_WORKERS', '2'))
+workers = int(os.getenv("GUNICORN_WORKERS", "2"))
 worker_class = os.getenv("GUNICORN_WORKER_CLASS", "uvicorn.workers.UvicornWorker")
 timeout = int(os.getenv("GUNICORN_TIMEOUT", "60"))
 accesslog = "-"
