bfa434d9fedbae9f2ecd120aa177758d2d4ae374
Viktor Kalistratov <64070252+Nefrit0n@users.noreply.github.com>
2026-04-22 17:24:49 +0300

    feat(loadtest): stdlib-only load testing harness with scenarios

---
+++ backend/internal/loadtest/real_cves.go
@@ -0,0 +1,20 @@
+package loadtest
+
+// RealCVEs is a curated list of well-known, valid CVE identifiers.
+var RealCVEs = []string{
+	"CVE-2021-44228",
+	"CVE-2021-45046",
+	"CVE-2021-45105",
+	"CVE-2022-22965",
+	"CVE-2022-22963",
+	"CVE-2019-11043",
+	"CVE-2021-26855",
+	"CVE-2020-1472",
+	"CVE-2017-5638",
+	"CVE-2021-41773",
+	"CVE-2021-42013",
+	"CVE-2021-3156",
+	"CVE-2018-11776",
+	"CVE-2021-26084",
+	"CVE-2022-26134",
+}
